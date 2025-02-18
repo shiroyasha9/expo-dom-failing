5d9b9344a747d7d6abda70443f5a14401c0b6490
Mubin Ansari <mubinansari910@gmail.com>
2025-02-18 22:44:48 +0530

    Add scrollview to test

--- components/TestDom.tsx
+++ components/TestDom.tsx
@@ -1,9 +1,18 @@
 "use dom";
 
-const TestDom = () => {
+import type { DOMProps } from "expo/dom";
+
+type Props = {
+	dom?: DOMProps;
+};
+
+const TestDom = (props: Props) => {
 	return (
 		<div>
 			<h1>This renders in the DOM? 🤯</h1>
+			{new Array(100).fill(0).map((_, index) => (
+				<div key={index.toString()}>Key {index}</div>
+			))}
 		</div>
 	);
 };
